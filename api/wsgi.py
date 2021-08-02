64d5c53a63d7e1aa32fb0e4d1c5737e0a66267a0
komura <kom314.prog@gmail.com>
2021-08-02 12:57:31 +0000

    fix: gunicornの設定に合わせてwsgi.pyを変更

--- api/wsgi.py
+++ api/wsgi.py
@@ -12,6 +12,6 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'little-village-api.settings')
 
 application = Cling(get_wsgi_application())
